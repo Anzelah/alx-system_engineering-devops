bd0d291a1ade8f520737fc106f8e4c819ebb69cc
Anzelah <jepkoechanzellah@gmail.com>
2023-09-22 07:20:44 -0700

    experimenting

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Kill a process
 
-exec { 'pkill killmenow':
-  command => '/usr/bin/pkill -f killmenow',
-  onlyif  => '/usr/bin/pkill -f killmenow'
+exec { 'kill':
+  command => '/usr/bin/pkill -f killmenow'
 }
