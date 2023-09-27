6e0a959a31cd9629aa56f1704ed290039a0469fe
Anzelah <jepkoechanzellah@gmail.com>
2023-09-27 05:06:27 -0700

    attempting advanced

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+# Edit your configuration file to do as task 3 above, but this time using puppet
+
+include stdlib
+
+package { 'nginx':
+provider => 'apt',
+}
+
+exec { 'hello'
+  command => '/usr/bin/sudo echo Hello World! /var/www/html/index.html'
+}
+
+exec { 'redirect_page':
+  command => '/usr/bin/sudo /bin/sed/ -i "30i location/redirect_me / {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}"/etc/nginx/sites-available/default'
+}
+
+exec { 'start_server':
+  command => '/usr/bin/sudo /usr/bin/service nginx start'
+}
