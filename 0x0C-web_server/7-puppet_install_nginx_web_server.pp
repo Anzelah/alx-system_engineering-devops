a929b8ca3ac05fe7e661c400e07caa71ffe4feb1
Anzelah <jepkoechanzellah@gmail.com>
2023-09-27 08:08:55 -0700

    trying advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,23 @@
 # Edit your configuration file to do as task 3 above, but this time using puppet
 
+
 package {'nginx':
-  ensure   => present,
+  ensure   => installed,
   provider => 'apt',
 }
 
-exec { 'installation':
-  command => 'sudo apt-get -y update; sudo apt-get install nginx',
-}
+#exec { 'installation':
+#  command => 'sudo apt-get -y update; sudo apt-get install nginx',
+
 
 exec { 'hello':
-  command => 'echo "Hello World!" > /var/www/html/index.html'
+  command => '/usr/bin/sudo echo "Hello World!" > /var/www/html/index.html'
 }
 
 exec { 'redirect_page':
-  command => 'sudo sed -i "listen 80 default_server;\\n\t30i location/redirect_me / {\\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;} "/etc/nginx/sites-available/default'
+  command => '/usr/bin/sudo /bin/sed/ -i "listen 80 default_server;\\n\t30i location/redirect_me / {\\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;} "/etc/nginx/sites-available/default'
 }
 
 exec { 'start_server':
-  command => 'sudo service nginx start'
+  command => '/usr/bin/sudo /usr/bin/ service nginx start'
 }
