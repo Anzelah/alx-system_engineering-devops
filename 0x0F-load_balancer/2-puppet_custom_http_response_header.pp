989a2f568fc6c4f02ca9ae864b4f9ce616f9d8bb
Anzelah <jepkoechanzellah@gmail.com>
2023-10-02 11:45:37 -0700

    fixed syntax errors and ran puppet apply

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,18 @@
 # Edit your configuration file to do as task 2 above, but this time using puppet
 
-exec { 'uppdate':
+exec { 'update':
   command => '/usr/bin/apt-get update'
 }
 
 package {'nginx':
-  ensure => present,
+  ensure => 'present',
 }
 
-file_line { 'Custom header':
-  path    => '/etc/nginx/sites-available/default'
-  line    => "server {\\\tadd_header X-Served-By $HOSTNAME;}"
-  replace => true
+file_line { 'Custom http header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => "server {\n\tadd_header X-Served-By $hostname;}"
 }
 
 exec { 'restart':
-  command => '/usr/bin/service nginx restart'
+  command => '/usr/sbin/service nginx restart'
 }
