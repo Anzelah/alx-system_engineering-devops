0cae7cb3f9e33c0e2631437fd22ebe8363df9070
Anzelah <jepkoechanzellah@gmail.com>
2023-10-25 12:55:10 -0700

    write data to csv for exporting

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+import csv
+import requests
+import sys
+
+
+def tabulate_tasks(eid):
+    """Populate tasks from the jsonplaceholder api"""
+    url = f"https://jsonplaceholder.typicode.com/users/{eid}"
+    res = requests.get(url)
+    users = res.json()
+    name = users.get("name")  # find employee name
+
+    url = f"https://jsonplaceholder.typicode.com/users/{eid}/todos"
+    res = requests.get(url)
+    tasks = res.json()
+
+    filename = ("{}.csv" .format(eid))
+    with open(filename, mode='a') as f:
+        for t in tasks:
+            completed = t.get("completed")
+            titles = t.get("title")
+            data = ("\"{}\",\"{}\",\"{}\",\"{}\"\n"
+                    .format(eid, name, completed, titles))
+
+            f.write(data)
+
+
+if __name__ == "__main__":
+    tabulate_tasks(sys.argv[1])
