18a39f6c29b30061febc3da0f2141e7d6cc8cd99
Anzelah <jepkoechanzellah@gmail.com>
2023-11-07 08:10:18 -0800

    retrieve got topics

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print titles of first 10 hot topics for a given subreddit"""
+
+    headers = {'User-Agent': 'MyApi/0.0.1'}
+    url = "https://api.reddit.com/r/{}/hot?limit=9".format(subreddit)
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        content = r.json()["data"]["children"]
+        for post in content:
+            print(post["data"]["title"])
+    else:
+        print(None)
