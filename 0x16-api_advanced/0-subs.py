b6088d83c4ad7e9047a23ea61dc7cf5348115ede
Anzelah <jepkoechanzellah@gmail.com>
2023-11-07 07:09:56 -0800

    Use right api url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Retuns number of subscribers for a given subreddit"""
+   
     headers =  {'User-Agent': 'MyApi/0.0.1'}
-    
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     res = requests.get(url, headers=headers)
 
     if res.status_code == 200:
         subs = res.json()["data"]["subscribers"]
+        return subs
     else:
-        subs = 0
-
-    return subs
+        return 0
