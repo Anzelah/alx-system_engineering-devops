10619b01d58ceb760c95817622e6850534755478
Anzelah <jepkoechanzellah@gmail.com>
2023-11-07 07:12:28 -0800

    pycodestyle update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Retuns number of subscribers for a given subreddit"""
-   
-    headers =  {'User-Agent': 'MyApi/0.0.1'}
+
+    headers = {'User-Agent': 'MyApi/0.0.1'}
     url = "https://api.reddit.com/r/{}/about".format(subreddit)
     res = requests.get(url, headers=headers)
 
