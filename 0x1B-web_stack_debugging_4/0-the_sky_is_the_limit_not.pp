2adb4a48b91420d2dedcf50606398d7a854d0381
Anzelah <jepkoechanzellah@gmail.com>
2023-11-18 09:27:38 -0800

    attempting to debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix server failing under pressure
 exec { 'limit':
-  onlyif   => 'test -e /etc/default/nginx/',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  onlyif  => 'test -e /etc/default/nginx/',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; sudo service nginx restart',
 }
