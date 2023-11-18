d6dcea6e58882682975b4e1123713baa88bb8e6e
Anzelah <jepkoechanzellah@gmail.com>
2023-11-18 09:23:19 -0800

    attempting to debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 exec { 'limit':
   onlyif   => 'test -e /etc/default/nginx/',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; sudo service nginx restart',
-  provider => 'shell',
+  provider => shell,
 }
